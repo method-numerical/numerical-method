08988a793bf2611376eff97d6061cb3594f1b723
numerical-method <subrata.panda@tifr.res.in>
2019-11-06 00:57:50 +0530

    converting to module that can compute n fibonacci numbers.

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,17 @@
 """
 assignment 1
-code to result first 10 fibonacci numbers
+module that can result any number of fibonacci number.
 
 """
+def fibonacci(n):
+    f=[0,1]
+    
+    for j in range(2,n):               #this list is [2,.......,n-1].
+        f.append(f[j-1]+f[j-2])        #any entry is sum of two entries before it.
+        j=j+1                          #initial terms of series.
 
-f=[0,1]                            #initial terms of series.
+    for k in range(0,n):
+        print('f(',k+1,')=',f[k])      #to print the numbers.
+        k=k+1
 
-for j in range(2,10):              #this list is [2,........9].
-    f.append(f[j-1]+f[j-2])        #any entry is sum of two entries before it.
-    j=j+1
-
-for k in range(0,10):
-    print('f(',k+1,')=',f[k])      #to print the numbers.
-    k=k+1
+fibonacci(100)
