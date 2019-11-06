d1e22c45640c628adde2ac3a44a28d9f8279b519
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-11-06 22:46:50 +0530

    code to result first 10 fibonacci numbers.

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+"""
+assignment 1
+to result first 10 fibonacci numbers.
+"""
+
+f=[0,1]                             #defining first 2 numbers.
+
+for j in range (2,10):              #this is a list from 2 to 9.
+    f.append(f[j-1]+f[j-2])         #any entry is sum of two entries before it
+    j=j+1
+
+for j in range (0,10):
+    print('f(',j+1,')=',f[j])
+    j=j+1
+print('completed.')
