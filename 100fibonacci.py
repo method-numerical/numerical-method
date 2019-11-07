3209681c296de1cbdbd3f6fa536817c1584b5bba
method-numerical <subrata.panda@mailhost.tifr.res.in>
2019-11-07 06:46:33 +0530

    using module to result first 100 fibonacci numbers and time needed to complete it.

--- 100fibonacci.py
+++ 100fibonacci.py
@@ -1,2 +1,16 @@
+"""
+importing fibonacci to result first 100 fiboacci numbers.
+using timeit() function to calculate time needed to complete it.
+"""
+import fibonacci
+import timeit
+
+a=fibonacci.fibonacci(100)
+
+code="""
 import fibonacci
 a=fibonacci.fibonacci(100)
+"""
+
+t=timeit.timeit(code,number=1)
+print('time needed in seconds :=',t,'.')
